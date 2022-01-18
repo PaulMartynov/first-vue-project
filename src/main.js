bdb01f51f078654b40f016038f4a357b411a9813
PaulMartynov <juanfen@yandex.ru>
2022-01-18 11:56:19 +0300

    Add axios to project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
+import Axios from 'axios';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -9,6 +10,8 @@ import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Axios.defaults.baseURL = process.env.API_ENDPOINT;
 
 Vue.use(BootstrapVue);
 
