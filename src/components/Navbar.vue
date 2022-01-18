ebc37088bef1cdbc2f7ded9fd18a26cfd5518b1d
PaulMartynov <juanfen@yandex.ru>
2022-01-18 11:36:12 +0300

    Add Navbar, Home-root & NotFound-root

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-navbar
+    toggleable="md"
+    type="dark"
+    variant="info">
+    <b-navbar-brand :to="{ name: 'Home' }">Home</b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item href="#">Second root</b-nav-item>
+      <b-nav-item href="#">Third root</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style scoped>
+.navbar {
+  padding: 5px 10px;
+}
+</style>
